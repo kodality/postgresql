e3d467d1db4e6515311d5fb515fb5ba6f42469b2
cerfy <mrcerf+git@gmail.com>
2018-06-12 12:01:51 +0300

    fix

--- publish.sh
+++ publish.sh
@@ -8,5 +8,5 @@ tag="10.3"
 #docker login || exit 1
 docker build -t $repo/$name . || exit 1
 
-docker tag $repo/$name $repo/$name:$tag &&\
+docker tag $repo/$name $repo/$name:$tag && \
 docker push $repo/$name:$tag
